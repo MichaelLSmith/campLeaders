9cbd7599c2ca833e0a08679cd36f41057422cc10
MichaelLSmith <mlsmith45@gmail.com>
2017-03-23 21:38:31 -0400

    working before camper to class component

--- src/leaderboard/Camper.js
+++ src/leaderboard/Camper.js
@@ -1,24 +1,22 @@
 import React from 'react';
-
-
-import { raw, campersData } from '../fetch';
+import { raw } from '../fetch';
 
 const Camper = () => {
-  console.log(campersData);
 
-  
   return (
-    <tr className="camper-row">
-      <th>1</th>
-      <th>
-        <img src={raw[0].img} alt=""/>
-        &nbsp;
-        <a href={`http://www.freecodecamp.com/${raw[0].username}`}>{raw[0].username}</a>
-      </th>
-      <th>{raw[0].recent}</th>
-      <th>{raw[0].alltime}</th>
-    </tr>
-  )
-}
+
+      <tr className="camper-row">
+        <th>1</th>
+        <th>
+          <img src={raw[0].img} alt=""/>
+          &nbsp;
+          <a href={`http://www.freecodecamp.com/${raw[0].username}`}>{raw[0].username}</a>
+        </th>
+        <th>{raw[0].recent}</th>
+        <th>{raw[0].alltime}</th>
+      </tr>
+    )
+  }
+
 
 export default Camper;
