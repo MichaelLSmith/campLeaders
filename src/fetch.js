092b797517899a434bbd781e652a1d552979ecb8
MichaelLSmith <mlsmith45@gmail.com>
2017-03-24 14:29:22 -0400

    working fetch: code

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,15 @@
-// const campersData =  fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-//     .then(response => response.json())
-//     .then(json => {console.log(json[0])});
+export const fetchCampersData = (setCampers) => {  fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    .then(response => response.json())
+    .then(campers => {
+
+      console.log(setCampers);
+      setCampers(campers)
+
+  });
+
+
+
+
+}
 
 export const raw = [{"username":"Manish-Giri","img":"https://avatars1.githubusercontent.com/u/11348778?v=3","alltime":4457,"recent":570,"lastUpdate":"2017-03-17T17:49:57.266Z"},{"username":"forkerino","img":"https://avatars.githubusercontent.com/u/16620061?v=3","alltime":1244,"recent":545,"lastUpdate":"2017-03-17T17:49:58.192Z"}]
--- src/leaderboard/Camper.js
+++ src/leaderboard/Camper.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { raw } from '../fetch';
+import React, { Component } from 'react';
+// import { raw } from '../fetch';
+// import { fetchCampersData } from '../fetch';
 
-const Camper = () => {
+export default class Camper extends Component {
+  constructor(){
+    super();
+    this.state = {campers: [ {"username":"forkerino","img":"https://avatars.githubusercontent.com/u/16620061?v=3","alltime":1244,"recent":545,"lastUpdate":"2017-03-17T17:49:58.192Z"}]}
+  }
 
-  return (
-
-      <tr className="camper-row">
-        <th>1</th>
-        <th>
-          <img src={raw[0].img} alt=""/>
-          &nbsp;
-          <a href={`http://www.freecodecamp.com/${raw[0].username}`}>{raw[0].username}</a>
-        </th>
-        <th>{raw[0].recent}</th>
-        <th>{raw[0].alltime}</th>
-      </tr>
+  setCampers(campers){
+    this.setState({ campers: campers }
     )
   }
 
+  fetchCampersData = () => {  fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(response => response.json())
+      .then(campers => {
+        this.setCampers(campers)
+    });
+  }
+  componentDidMount(){
+    console.log(this);
+    this.fetchCampersData(this.setCampers);
+  }
 
-export default Camper;
+  render () {
+    const { campers } = this.state;
+
+
+
+    return (
+      <tr className="camper-row">
+        <th>1</th>
+        <th>
+          <img src={campers[0].img} alt=""/>
+          &nbsp;
+          <a href={`http://www.freecodecamp.com/${campers[0].username}`}>{campers[0].username}</a>
+        </th>
+        <th>{campers[0].recent}</th>
+        <th>{campers[0].alltime}</th>
+      </tr>
+    );
+  }
+}
